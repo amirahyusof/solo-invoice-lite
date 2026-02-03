4d2527e5f9befd7e8d9dd25c17cd111910a0ec17
Amirah Yusof <mirez.data@gmail.com>
2026-02-03 17:12:31 +0800

    commit changes due to pwa error

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,6 +13,10 @@ self.addEventListener("install", (event) => {
   );
 });
 
+self.addEventListener("activate", () => {
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
