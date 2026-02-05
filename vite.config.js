36c9a1e0461a5b9a28fa457dde6bc252f678344e
Amirah Yusof <mirez.data@gmail.com>
2026-02-05 12:03:51 +0800

    commit all changes

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
+      strategies: 'generateSW',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
       manifest: {
         name: 'SoloInvoice Lite',
